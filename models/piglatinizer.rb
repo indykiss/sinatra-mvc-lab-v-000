dd865f8131a8b67f3fe8c67143bc12ba3dbdc624
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-01-18 03:20:14 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -16,7 +16,7 @@ class PigLatinizer
   
   def piglatinize(user_input)
     string_array = user_input.split(" ")
-    constants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
+    constants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z', "B", "C", "D", "F", "G", "H", "I", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Z"]
     vowels = ["a","e","i","o","u", "A", "E", "I", "O", "U"]
     
     string_array.map do |word|
