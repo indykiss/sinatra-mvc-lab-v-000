221d5f15cd8f0caf4b03f02a2d8ac30bbf6b6c0d
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-01-18 03:04:35 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
   
   post '/piglatinize' do 
     @piglatinizer = Piglatinizer.new
-    @user_phrase = @Piglatinizer(params[:user_phrase])
+    @user_phrase = @Piglatinizer,piglatinize(params[:user_phrase])
 
     erb :display_piglatin
   end 
